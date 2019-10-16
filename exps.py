dab258c27edc6c34ae0d203f1d230227f4246885
4teraflops <woolf1514@riseup.net>
2019-10-16 17:46:46 +0500

    добавлены два исключения

--- exps.py
+++ exps.py
@@ -1,17 +1,48 @@
+import time
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-
-#url = ('https://ckassa.ru/payment/#!search_provider/pt_search/' + '{}' + '/pay').format(service_code)
-
-
-with open('cods.txt', 'rU') as f:#читаем список из файла и превращаем его в список
-    service_cods = f.read().split('\n')
+test_res = {}
 urls = []
-for s in service_cods:#теперь составляем список ссылок, которые будем тестить
-    url = ('https://ckassa.ru/payment/#!search_provider/pt_search/' + '{}' + '/pay').format(s)
-    urls.append(url)
 
 
+def create_urls_list():
+    print("Составляю список ссылок для парсинга...", end="")
+    with open('cods.txt', 'rU') as f:#читаем содержимое файла и превращаем его в список
+        service_cods = f.read().split('\n')
+    for s in service_cods:#теперь составляем список ссылок, которые будем тестить
+        url = ('https://ckassa.ru/payment/#!search_provider/pt_search/' + '{}' + '/pay').format(s)
+        urls.append(url)
+    print(" Ок")
 
 
+def check_urls():
+    driver = webdriver.Chrome('/home/sanaev-va/chromedriver')  # указал где брать гугл хром драйвер
+    driver.implicitly_wait(10)# неявное ожидание драйвера
+    driver.get(url)
+    input_ls = driver.find_element_by_css_selector('input[type=text]')#нашел форму по css selector (скопировал из браузера)
+    input_ls.send_keys('9659659659')#ввел несуществующий л/с
+    input_ls.send_keys(Keys.TAB)#переключился на следующее поле
+    wait = WebDriverWait(driver, 30)#Задал переменную, чтоб настроить явное ожидание элемента (сек)
+    output_element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="payMasksBlock"]/div/div[1]/div')))#ждем, пока элемент прогрузится
+    output_text = output_element.text.split('\n')  # парсим из него текст
+    print(f'{url} - {output_text}')#выводим
+    test_res[url] = output_text#записываем его в общий список ответов
+    driver.close()
+
+
+if __name__ == "__main__":
+    try:
+        create_urls_list()
+        for url in urls:#запуск перебора всех составленных ссылок
+            check_urls()
+        for key, value in test_res.items():
+            print(key, value)
+    except KeyboardInterrupt:
+        print('Вы завершили работу программы. Закрываюсь.')
 
 
--- postmon.py
+++ postmon.py
@@ -1,34 +1,56 @@
 import time
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 test_res = {}
-with open('cods.txt', 'rU') as f:#читаем список из файла и превращаем его в список
-    service_cods = f.read().split('\n')
 urls = []
-for s in service_cods:#теперь составляем список ссылок, которые будем тестить
-    url = ('https://ckassa.ru/payment/#!search_provider/pt_search/' + '{}' + '/pay').format(s)
-    urls.append(url)
 
 
-for url in urls:
-    driver = webdriver.Chrome('/home/sanaev-va/chromedriver')
+def create_urls_list():
+    print("Составляю список ссылок для парсинга...", end="")
+    with open('cods.txt', 'rU') as f:#читаем содержимое файла и превращаем его в список
+        service_cods = f.read().split('\n')
+    for s in service_cods:#теперь составляем список ссылок, которые будем тестить
+        url = ('https://ckassa.ru/payment/#!search_provider/pt_search/' + '{}' + '/pay').format(s)
+        urls.append(url)
+    print(" Ок")
+
+
+def check_urls():
+    driver = webdriver.Chrome('/home/sanaev-va/chromedriver')  # указал где брать гугл хром драйвер
+    driver.implicitly_wait(10)# неявное ожидание драйвера
+    wait = WebDriverWait(driver, 10)  # Задал переменную, чтоб настроить явное ожидание элемента (сек)
     driver.get(url)
-    time.sleep(1.5)
-    input_ls = driver.find_element_by_css_selector('input[type=text]')#нашел форму по css selector (скопировал из браузера)
-    time.sleep(0.5)
-    input_ls.send_keys('965557449')#ввел несуществующий л/с
-    input_ls.send_keys(Keys.TAB)#переключился на следующее поле
-    time.sleep(1)
-    output_element = driver.find_element_by_xpath('//*[@id="payMasksBlock"]/div/div[1]/div')#ловим элемент с результатами теста
-    output_text = output_element.text.split('\n')#парсим из него текст
-    print(f'{url} - {output_text}')
-    test_res[url] = output_text#записываем его в общий список ответов
+    try:
+        input_ls = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="payMasksBlock"]/div/div[2]/input')))#нашел форму по XPATH
+        input_ls.send_keys('9659659659')  # ввел несуществующий л/с
+        input_ls.send_keys(Keys.TAB)  # переключился на следующее поле
+    except TimeoutException:#если не удалось найти форму, генерим исключение и записываем его в общий результат
+        print(f'{url} - не удалось спарсить элемент input(таймаут ожидания)')
+        test_res[url] = 'не удалось спарсить элемент input(таймаут ожидания)'  #и записываем его в общий список результатов
+
+    try:
+        output_element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="payMasksBlock"]/div/div[1]/div')))#ждем, пока элемент прогрузится
+        output_text = output_element.text.split('\n')  # парсим из него текст
+        print(f'{url} - {output_text}')  # выводим то что собрал
+        test_res[url] = output_text  # записываем его в общий список ответов
+    except TimeoutException:#если по таймауту собрать не удалось, выводим исключение
+        print(f'{url} - не удалось спарсить элемент gwt-Label(таймаут ожидания)')
+        test_res[url] = 'не удалось спарсить элемент gwt-Label(таймаут ожидания)'  #и записываем его в общий список результатов
     driver.close()
 
-print(test_res)
 
-
-#    def TearDown(self):
-#        self.driver.close()
-
+if __name__ == "__main__":
+    try:
+        create_urls_list()
+        for url in urls:#запуск перебора всех составленных ссылок
+            check_urls()
+        print('Результаты теста: \n')
+        for key, value in test_res.items():
+            print(key, value)
+    except KeyboardInterrupt:
+        print('Вы завершили работу программы. Закрываюсь.')
