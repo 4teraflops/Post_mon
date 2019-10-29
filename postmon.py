e310aae0b4bb7ecbb1848a7927ad0af8b68d4751
4teraflops <woolf1514@riseup.net>
2019-10-29 11:28:27 +0500

    доделана функция распределения первичных результатов теста

--- postmon.py
+++ postmon.py
@@ -50,7 +50,7 @@ t = timeit.default_timer()#переменная для замера времен
 
 def create_urls_list():
     print("Составляю список ссылок для парсинга...", end="")
-    with open('cods1.txt', 'rU') as f:#читаем содержимое
+    with open('cods.txt', 'rU') as f:#читаем содержимое
         service_cods = f.read().split('\n')#читаем пропуская перенос строки
     for s in service_cods:#теперь составляем список ссылок, которые будем тестить
         url = ('https://ckassa.ru/payment/#!search_provider/pt_search/' + '{}' + '/pay').format(s)#превращаем код услуги в ссылку для теста
@@ -77,7 +77,7 @@ def check_urls():
         except TimeoutException:#если не удалось найти форму, генерим исключение и записываем его в общий результат
             try:#ищем лого сайта (чтоб отличить загруженнуб страницу без услуги от незагруженной страницы)
                 driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/table/tbody/tr/td[1]/div/img')
-            except Exception:#если лого нет, то страница не прогрузилась
+            except Exception:#если лого нет, значит страница не прогрузилась
                 print(f'{url} - {c}')
                 first_test_res[url] = c
                 driver.close()
